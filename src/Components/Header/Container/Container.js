e30b8d96365c85d462d12158572701a287829112
MuratkanDamir <muratkandamir00@gmail.com>
2023-10-12 15:19:30 +0500

    add header's container

--- src/Components/Header/Container/Container.js
+++ src/Components/Header/Container/Container.js
@@ -1,22 +1,30 @@
-import {Button, Card} from "react-bootstrap";
+import {Button, Card, Image, NavLink} from "react-bootstrap";
 import styles from "./Container.module.css";
+import curveImg from "../../../Source/Curve.svg"
 function Container(){
     return(
-        <div className="container bg-opacity-0">
-           <Card style={{ width: '50%'}}>
-                <Card.Body>
-                    <Card.Title>We help you</Card.Title>
-                    <Card.Title>Grow your busines faster</Card.Title>
-                    <Card.Text>
-                    Some quick example text to build on the card title and make up the
-                    bulk of the card's content.
+        <div className="container">
+           <Card className="col-12 col-md-7 border border-0" bg="transparent" border="transparent">
+                <Card.Body className="mt-2 mt-md-3 mt-lg-4">
+                    <Card.Title style={{ fontSize: '65px', color: "white"}}>We help you</Card.Title>
+                    <Card.Title style={{ fontSize: '65px', color: "white" }}>grow your business faster</Card.Title>
+                    <Image src={curveImg}></Image>
+                    <Card.Text className="mt-3" style={{color: "white" }}>
+                    Ehya is the Instagram analytics platform teams use to stay focused on the goals, track engagement for report your business .
                     </Card.Text>
-                    <Button variant="primary">Go somewhere</Button>
-                    <Button variant="primary">Go somewhere</Button>
+                    <div style={{ display: "flex", gap: "40px", marginTop: "30px"}}>
+                        <Button  size="lg" variant="light" className="rounded-1" style={{ fontSize: "1em",color: '#00A8DB'}}>See how it works</Button>
+                        <NavLink className="btn btn-secondary d-flex align-items-center" style={{paddingLeft: "15px", fontSize: "1em",color: 'white'}}> 
+                        Get a free demo 
+                        <div class={styles.arrow1}>
+                            <div></div>
+                        </div>
+                        </NavLink>    
+                    </div>
                 </Card.Body>
             </Card> 
         </div>
     );
 };
-
+// style={{ width: '50%', minWidth: '300px' }}
 export default Container;
